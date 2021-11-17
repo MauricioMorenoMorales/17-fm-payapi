37b06a07d1d8c3802c24d4ef20ebc2c2a071f4c6
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-17 08:58:28 -0600

    Base styles added

---
+++ src/global/designSystem.ts
@@ -0,0 +1,8 @@
+export const colorSystem = {
+	darkPink: '#ba4270',
+	linkWaterWhite: '#fbfcfe',
+	sanJuanBlue: '#36536b',
+	mirageBlue: '#1b262f',
+	charmPinkActive: '#da6d97',
+	lightSanJuanBlue: '#6c8294',
+};
