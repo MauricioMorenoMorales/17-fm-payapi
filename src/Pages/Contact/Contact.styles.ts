ee6635f4d95970e0148106313903ec9cf2e4c35e
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-12-01 22:34:30 -0600

    Contact section completed

--- src/Pages/Contact/Contact.styles.ts
+++ src/Pages/Contact/Contact.styles.ts
@@ -1,5 +1,79 @@
 import styled from 'styled-components';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+	padding-top: 130px;
+	& > h2 {
+		text-align: center;
+		max-width: 390px;
+		margin: 0 auto;
+		padding: 20px;
+	}
+	.container {
+		width: 100%;
+		padding: 25px;
+	}
+	& form {
+		width: 100%;
+		max-width: 370px;
+		margin: 0 auto;
+		& > button {
+			margin-top: 20px;
+		}
+	}
+	.companies {
+		text-align: center;
+		max-width: 370px;
+		margin: 0 auto;
+		margin-top: 100px;
+		&__logos {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			gap: 20px;
+			justify-items: center;
+			align-items: center;
+			& > img {
+				margin-bottom: 20px;
+			}
+		}
+	}
+	@media screen and (min-width: 768px) {
+		h2 {
+			max-width: 490px;
+		}
+		& form {
+			max-width: 460px;
+		}
+	}
+	.companies {
+		max-width: 560px;
+		&__logos {
+			grid-template-columns: 1fr 1fr 1fr;
+		}
+	}
+	@media screen and (min-width: 1100px) {
+		max-width: 1100px;
+		margin: 0 auto;
+		h2 {
+			text-align: start;
+			max-width: 900px;
+			margin-left: 0;
+		}
+		.container {
+			display: grid;
+			grid-template-columns: 460px auto;
+			gap: 50px;
+			max-width: 1100px;
+			margin: 0 auto;
+			align-items: center;
+		}
+		.companies {
+			text-align: start;
+			margin-top: -50px;
+			& > h4 {
+				margin-left: 15px;
+			}
+		}
+	}
+`;
 
 export default Styles;
--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import Styles from './Contact.styles';
+// Logos
+import Tesla from '../../assets/shared/desktop/tesla.svg';
+import Microsoft from '../../assets/shared/desktop/microsoft.svg';
+import HP from '../../assets/shared/desktop/hewlett-packard.svg';
+import Oracle from '../../assets/shared/desktop/oracle.svg';
+import Google from '../../assets/shared/desktop/google.svg';
+import Nvidia from '../../assets/shared/desktop/nvidia.svg';
 
 const Contact = () => {
 	const [invalidEmail, setInvalidEmail] = useState<boolean>(false);
@@ -8,68 +15,101 @@ const Contact = () => {
 	const [invalidTitle, setInvalidTitle] = useState<boolean>(false);
 	const [invalidMessage, setInvalidMessage] = useState<boolean>(false);
 
+	const handleSubmit = (event: React.SyntheticEvent) => {
+		if (
+			invalidEmail ||
+			invalidName ||
+			invalidCompany ||
+			invalidTitle ||
+			invalidMessage
+		) {
+			event.preventDefault();
+			return;
+		} else {
+			alert('Thanks for the Message :)');
+		}
+	};
+
 	return (
 		<Styles>
 			<h2 className="big">
 				Submit a help request and we'll get in touch shortly
 			</h2>
-			<form>
-				<input
-					type="text"
-					placeholder="Name"
-					title="Name"
-					className={`${invalidName && 'active'}`}
-					onChange={event => setInvalidName(!(event.target.value.length >= 3))}
-				/>
-				<p className={`input-error ${invalidName && 'active'}`}>
-					Name should be at least 3 Characters
-				</p>
-				<input
-					type="text"
-					className={`${invalidEmail && 'active'}`}
-					placeholder="Email Address"
-					onChange={event =>
-						setInvalidEmail(
-							!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-								event.target.value,
-							),
-						)
-					}
-				/>
-				<p className={`input-error ${invalidEmail && 'active'}`}>
-					Invalid Email
-				</p>
-				<input
-					type="text"
-					placeholder="Company Name"
-					className={`${invalidCompany && 'active'}`}
-					onChange={event =>
-						setInvalidCompany(!(event.target.value.length >= 3))
-					}
-				/>
-				<p className={`input-error ${invalidCompany && 'active'}`}>
-					Company Name should contain at least 3 letters
-				</p>
-				<input
-					type="text"
-					placeholder="Title"
-					className={`${invalidTitle && 'active'}`}
-					onChange={event => setInvalidTitle(!(event.target.value.length >= 3))}
-				/>
-				<p className={`input-error ${invalidTitle && 'active'}`}>
-					Title must contain at least 3 characters
-				</p>
-				<textarea
-					placeholder="Message"
-					className={`${invalidMessage && 'active'}`}
-					onChange={event =>
-						setInvalidMessage(!(event.target.value.length > 0))
-					}
-				/>
-				<p className={`input-error ${invalidMessage && 'active'}`}>
-					This field can't be empty
-				</p>
-			</form>
+			<div className="container">
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						placeholder="Name"
+						title="Name"
+						className={`${invalidName && 'active'}`}
+						onChange={event =>
+							setInvalidName(!(event.target.value.trim().length >= 3))
+						}
+					/>
+					<p className={`input-error ${invalidName && 'active'}`}>
+						Name should be at least 3 Characters
+					</p>
+					<input
+						type="text"
+						className={`${invalidEmail && 'active'}`}
+						placeholder="Email Address"
+						onChange={event =>
+							setInvalidEmail(
+								!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+									event.target.value,
+								),
+							)
+						}
+					/>
+					<p className={`input-error ${invalidEmail && 'active'}`}>
+						Invalid Email
+					</p>
+					<input
+						type="text"
+						placeholder="Company Name"
+						className={`${invalidCompany && 'active'}`}
+						onChange={event =>
+							setInvalidCompany(!(event.target.value.trim().length >= 3))
+						}
+					/>
+					<p className={`input-error ${invalidCompany && 'active'}`}>
+						Company Name should contain at least 3 letters
+					</p>
+					<input
+						type="text"
+						placeholder="Title"
+						className={`${invalidTitle && 'active'}`}
+						onChange={event =>
+							setInvalidTitle(!(event.target.value.trim().length >= 3))
+						}
+					/>
+					<p className={`input-error ${invalidTitle && 'active'}`}>
+						Title must contain at least 3 characters
+					</p>
+					<textarea
+						placeholder="Message"
+						className={`${invalidMessage && 'active'}`}
+						onChange={event =>
+							setInvalidMessage(!(event.target.value.trim().length > 0))
+						}
+					/>
+					<p className={`input-error ${invalidMessage && 'active'}`}>
+						This field can't be empty
+					</p>
+					<button className="secondary-dark">Submit</button>
+				</form>
+				<article className="companies">
+					<h4>Join the thousands of innovators already building with us</h4>
+					<div className="companies__logos">
+						<img src={Tesla} alt="Tesla" />
+						<img src={Microsoft} alt="Microsoft" />
+						<img src={HP} alt="Hewlett Packard" />
+						<img src={Oracle} alt="Oracle" />
+						<img src={Google} alt="Google" />
+						<img src={Nvidia} alt="Nvidia" />
+					</div>
+				</article>
+			</div>
 		</Styles>
 	);
 };
